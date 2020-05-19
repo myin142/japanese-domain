7b68c8174d7ad3943ae5a92d0a57571d85a175d0
myin <minyin142p@gmail.com>
2020-05-19 20:48:24 +0200

    Change to history mode

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -9,60 +9,60 @@ import { auth } from '@/services/auth.service';
 Vue.use(VueRouter);
 
 const authGuard = (to: any, from: any, next: any) => {
-	if (auth.isAuthenticated()) {
-		next();
-	} else {
-		next({ name: 'Login' });
-	}
+    if (auth.isAuthenticated()) {
+        next();
+    } else {
+        next({ name: 'Login' });
+    }
 };
 
 const handleAuth = async (to: any, from: any, next: any) => {
-	const currUrl = window.location.href;
-	await auth.handleAuthCallback(currUrl);
-	const redirect = auth.isAuthenticated() ? 'Radicals' : 'Error';
-	next({ name: redirect });
+    const currUrl = window.location.href;
+    await auth.handleAuthCallback(currUrl);
+    const redirect = auth.isAuthenticated() ? 'Radicals' : 'Error';
+    next({ name: redirect });
 }
 
 const routes: Array<RouteConfig> = [
-	{
-		path: '/',
-		redirect: '/radicals',
-	},
-	{
-		path: '/radicals',
-		name: 'Radicals',
-		component: Radicals,
-		beforeEnter: authGuard,
-	},
-	{
-		path: '/grammar',
-		name: 'Grammar',
-		component: Grammar,
-	},
-	{
-		path: '/login',
-		name: 'Login',
-		component: Login,
-	},
-	{
-		path: '/login/oauth2',
-		beforeEnter: handleAuth,
-	},
-	// Auth for local development
-	{
-		path: '/id_token=*',
-		beforeEnter: handleAuth,
-	},
-	{
-		path: '**',
-		name: 'Error',
-		component: Error,
-	},
+    {
+        path: '/',
+        redirect: '/radicals',
+    },
+    {
+        path: '/radicals',
+        name: 'Radicals',
+        component: Radicals,
+        beforeEnter: authGuard,
+    },
+    {
+        path: '/grammar',
+        name: 'Grammar',
+        component: Grammar,
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login,
+    },
+    {
+        path: '/login/oauth2',
+        beforeEnter: handleAuth,
+    },
+    // Auth for local development
+    {
+        path: '/id_token=*',
+        beforeEnter: handleAuth,
+    },
+    {
+        path: '**',
+        name: 'Error',
+        component: Error,
+    },
 ];
 
 const router = new VueRouter({
-	base: `/${process.env.VUE_APP_BASE_URL}`,
-	routes,
+    mode: "history",
+    routes,
 });
 
 export default router;
