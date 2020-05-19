54b20a59880342e8a89a08ea2fcff1413d1e0625
myin <minyin142p@gmail.com>
2020-05-19 21:04:03 +0200

    Another workaround for spa on static hosting

--- client/src/main.ts
+++ client/src/main.ts
@@ -6,12 +6,19 @@ import * as AWS from 'aws-sdk';
 
 AWS.config.region = 'eu-central-1';
 AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-	IdentityPoolId: 'eu-central-1:8a5af73f-4932-492c-b9ff-573e50aa3f79',
+    IdentityPoolId: 'eu-central-1:8a5af73f-4932-492c-b9ff-573e50aa3f79',
 });
 
 Vue.config.productionTip = false;
 
 new Vue({
-	router,
-	render: h => h(App)
+    router,
+    render: h => h(App),
+    created() {
+        if (sessionStorage.redirect) {
+            const redirect = sessionStorage.redirect
+            delete sessionStorage.redirect
+            this.$router.push(redirect)
+        }
+    }
 }).$mount("#app");
