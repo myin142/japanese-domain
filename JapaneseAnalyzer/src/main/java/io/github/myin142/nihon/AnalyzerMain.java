6a1a88f72f5ac16b8eb3761c749139f390492f19
myin <minyin142p@gmail.com>
2020-06-04 21:30:14 +0200

    Add JapaneseAnalyzer

---
+++ JapaneseAnalyzer/src/main/java/io/github/myin142/nihon/AnalyzerMain.java
@@ -0,0 +1,20 @@
+package io.github.myin142.nihon;
+
+import java.util.LinkedHashMap;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.atilika.kuromoji.unidic.Tokenizer;
+
+/**
+ * Handler for requests to Lambda function.
+ */
+public class AnalyzerMain implements RequestHandler<LinkedHashMap<String, String>, Object> {
+
+    public Object handleRequest(final LinkedHashMap<String, String> input, final Context context) {
+        String word = input.get("q");
+        Tokenizer tokenizer = new Tokenizer();
+        return tokenizer.tokenize(word);
+    }
+
+}
